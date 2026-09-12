f0a9a97817174c27e7acec6783e5cae92d6db07e
a-barret <aidenb360@gmail.com>
2026-09-11 23:42:32 -0600

    Added an automatic check for dark mode or light mode so that neovim matches the system setting.

--- lua/custom/plugins/gruvbox.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add({"https://github.com/ellisonleao/gruvbox.nvim"})
-
-require('gruvbox').setup {
-  contrast = 'hard',
-  terminal_colors = true,
-}
-
-vim.o.background = 'dark'
-vim.cmd.colorscheme 'gruvbox'
