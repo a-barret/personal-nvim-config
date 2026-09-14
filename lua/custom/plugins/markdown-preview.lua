4ad9c6c8875e244337d81e134cb0fe550144ead7
a-barret <aidenb360@gmail.com>
2026-09-14 16:28:39 -0600

    Switched markdown preview tool from markview to markdown-preview to allow for mermaid chart preview support.

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,9 +1,12 @@
-vim.pack.add({ 'https://github.com/OXY2DEV/markview.nvim' })
+vim.pack.add({
+  { src = "https://github.com/iamcco/markdown-preview.nvim", load = false },
+})
 
-local config = {
-    preview = {
-        enable = false,
-    },
-}
-
-require('markview').setup(config)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.cmd.packadd("markdown-preview.nvim")
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", { buffer = true })
+  end,
+})
